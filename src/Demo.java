ede65ed988df4138425b58b0ee0ed29e3d36fc8c
manoj <manoj.sarwate@ssiedu.in>
2021-03-13 13:30:46 +0530

    130321

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int ar[]={10,20,30,40,50};
+        
+        //an array is a not-premitive-data-type(object)
+        
+        System.out.println(ar[0]);  //1st element 
+        System.out.println(ar[2]);
+        
+        System.out.println(ar); //it will be displayed as an object
+        
+        //when we print an object, internally toString method will be called.
+        
+            
+        
+        
+        
+    }
+}
--- src/VArgExample.java
+++ src/VArgExample.java
@@ -1,21 +1,52 @@
-
+class Trial{}
 public class VArgExample {
 
+    public static void add(){
+        System.out.println("Nothing-To-Add");
+    }
+    public static void add(double a,double b){
+        System.out.println("SUM : "+(a+b));
+    }
+    public static void add(String... v){
+        int tot=v.length;
+        System.out.println("Total Elements Passed : "+tot);
+        String res="";
+        for(String s:v){
+            res=res+s;
+        }
+        System.out.println(res);
+    }
     public static void add(int... v){
+        
         int sum=0;
         for(int num:v){
             sum=sum+num;
         }
         System.out.println(sum);
+        
     }
     
     public static void main(String[] args) {
         
-        add();      //{}
-        add(10,20);
+        add();
+        add(10,20); 
+        add("aa","bb");
+        add("xx","yy","zz");
         add(10,20,30);
         add(10,20,30,40);
-        add(1,2,3,4,5,6,7,8,9,10);
         
     }
 }
+
+/*
+    some points to remember for using varargs
+    
+    1) only one varargs per method.
+    2) normal parameters (non-varargs)  can be taken with varargs
+    3) varargs must be always on last position
+    4) varargs methods can be overloaded.
+    5) preference is always given to exact match.
+
+
+
+*/
